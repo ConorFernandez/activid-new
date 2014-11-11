a2324ac907be3433703d36c1b1b754211b2fe5b6
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-11-11 13:44:59 -0600

    step navigation for project form

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -11,4 +11,21 @@ module ProjectsHelper
       [name, k]
     end
   end
+
+  def step_navigation_link(project, step_name, active)
+    if !project.persisted? || active
+      content = step_name
+    else
+      step_path = case step
+                  when 1 then project_step1_path(project)
+                  when 2 then project_step2_path(project)
+                  when 3 then project_step3_path(project)
+                  when 4 then project_step4_path(project)
+                  end
+
+      content = link_to step_name, step_path
+    end
+
+    "<li style=\"display: inline-block; margin-right: 1em\">#{content}</li>".html_safe
+  end
 end
