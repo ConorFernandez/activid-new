6911feec2a8c04c6f6430a2108fd5203016d845a
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-11-20 15:34:59 -0600

    added projects/update action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,5 @@
 class ProjectsController < ApplicationController
-  before_filter :load_project, only: [:step1, :step2, :step3, :step4]
+  before_filter :load_project, only: [:update, :step1, :step2, :step3, :step4]
 
   def new
     @project = Project.new
@@ -9,9 +9,17 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
 
     if @project.save
-      redirect_to next_step(@project)
+      redirect_to next_step_path(@project)
     else
-      render :step1
+      render current_step_action
+    end
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to next_step_path(@project)
+    else
+      render current_step_action
     end
   end
 
@@ -28,19 +36,28 @@ class ProjectsController < ApplicationController
   private
 
   def project_params
-    params.require(:project).permit(:name, :category, :desired_length, :instructions, :allow_to_be_featured)
+    if params[:project].present?
+      params.require(:project).permit(:name, :category, :desired_length, :instructions, :allow_to_be_featured)
+    else
+      {}
+    end
   end
 
   def load_project
     @project = Project.where(uuid: params[:id]).first
   end
 
-  def next_step(project)
+  def next_step_path(project)
     case params[:step] ? params[:step].to_i : nil
     when 1 then project_step2_path(project)
     when 2 then project_step3_path(project)
     when 3 then project_step4_path(project)
     else root_path
     end
+  end
+
+  def current_step_action
+    step = params[:step].to_i
+    (1..4).include?(step) ? "step#{step}".to_sym : raise("Could not determine current step")
   end
 end
