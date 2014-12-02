2ae138efede390fbd98705de9ef6c83e9e24432a
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-12-02 10:52:35 -0600

    capture name during signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :html, :json
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:full_name, :email, :password, :password_confirmation)
+  end
 end
