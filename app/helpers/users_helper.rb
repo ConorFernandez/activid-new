a68db613a492edeea4252d06e764b1ebfdb9c9bc
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-12-10 16:32:42 -0600

    show user an alert on project page if waiting for first cut

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,11 @@ module UsersHelper
   include Avatar::View::ActionViewSupport
 
   def display_name_for_user(user)
-    user.full_name.blank? ? user.email : user.full_name
+    user.full_name.blank? ? user.email : user.full_name.split(" ").first
+  end
+
+  def long_display_name_for_user(user)
+    user.full_name.presence || user.email
   end
 
   def gravatar_url_for(user)
