c3ec4b19ada3affcc593034e79e2538fd48c96fb
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-12-30 12:55:45 -0600

    start on single editor admin page

--- app/controllers/editors_controller.rb
+++ app/controllers/editors_controller.rb
@@ -1,6 +1,7 @@
 class EditorsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :ensure_user_is_admin
+  before_filter :load_editor, only: :show
 
   def index
     @editors = User.editor.all
@@ -12,5 +13,10 @@ class EditorsController < ApplicationController
     unless current_user && current_user.admin?
       return redirect_to(root_path)
     end
+  end
+
+  def load_editor
+    @editor = User.where(id: params[:id]).first
+    raise ActiveRecord::RecordNotFound unless @editor && @editor.editor?
   end
 end
