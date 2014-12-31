91b86c68f6960bb5c7d065ee10d7d6aea276e1a8
Joey Schoblaska <joey.schoblaska@gmail.com>
2014-12-31 15:20:12 -0600

    validate that a cut has an attachment

--- app/models/cut.rb
+++ app/models/cut.rb
@@ -4,6 +4,8 @@ class Cut < ActiveRecord::Base
 
   has_one :file_upload, as: :attachable
 
+  validate :has_upload
+
   def processed?
     file_upload.zencoder_finished? && file_upload.preview_url.present?
   end
@@ -18,5 +20,11 @@ class Cut < ActiveRecord::Base
 
   def zencoder_error
     file_upload.try(:zencoder_error)
+  end
+
+  private
+
+  def has_upload
+    errors.add(:base, "Must have an attached file") unless file_upload.present?
   end
 end
--- test/controllers/cuts_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CutsControllerTest < ActionController::TestCase
-  test "POST create only creates a cut if at least one upload is supplied" do
-    skip
-  end
-end
