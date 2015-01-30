3de773fc432bc827ff43e16a019ae6be84704264
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-01-30 16:14:45 -0600

    finish new editor application form

--- app/controllers/editor_applications_controller.rb
+++ app/controllers/editor_applications_controller.rb
@@ -1,10 +1,10 @@
 class EditorApplicationsController < ApplicationController
-  PARAM_KEYS = [:full_name, :email, :bio, :portfolio]
+  PARAM_KEYS = [:full_name, :email, :city, :state, :country, :bio, :portfolio_1, :portfolio_2, :portfolio_3]
 
   def create
     if PARAM_KEYS.any?{|k| params[k].blank?}
-      flash[:error] = "Please fill out the form completely."
-      redirect_to new_editor_application_path
+      flash.now[:error] = "Please fill out the form completely."
+      render action: :new
     else
       Mailer.editor_application_email(params.slice(*PARAM_KEYS)).deliver
       render action: :thank_you
