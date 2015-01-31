e456a6c8acb8bc65c326c00504639bb0ce4690e9
Joey Schoblaska <joey.schoblaska@gmail.com>
2015-01-30 18:18:23 -0600

    don't require state (for our international friends)

--- app/controllers/editor_applications_controller.rb
+++ app/controllers/editor_applications_controller.rb
@@ -2,7 +2,9 @@ class EditorApplicationsController < ApplicationController
   PARAM_KEYS = [:full_name, :email, :city, :state, :country, :bio, :portfolio_1, :portfolio_2, :portfolio_3]
 
   def create
-    if PARAM_KEYS.any?{|k| params[k].blank?}
+    required_keys = PARAM_KEYS - [:state]
+
+    if required_keys.any?{|k| params[k].blank?}
       flash.now[:error] = "Please fill out the form completely."
       render action: :new
     else
