feca011ecf279f48d1f5685bd9632a80cd7be584
jonathan-eyler-werve <jonathan@eylerwerve.com>
2015-03-05 14:25:25 -0600

    bugfix in file download buttons

--- app/assets/javascripts/file_download_helper.js
+++ app/assets/javascripts/file_download_helper.js
@@ -3,7 +3,7 @@
 var downloadAll; // takes a JQuery selector as string such as ".downloadable" or ".downloadable-image"
 
 downloadAll = function (event) { 
-  event.preventDefault; 
+  event.preventDefault(); 
   $(event.data.selector).multiDownload({ delay: 1000 }); // delay prevents "multiple file downloads: allow? yes/no" warning in some browsers
   $(event.data.selector).css("background-color", "yellow");
   setTimeout(function(){ $(event.data.selector).css("background-color", "inherit");;}, 250);
